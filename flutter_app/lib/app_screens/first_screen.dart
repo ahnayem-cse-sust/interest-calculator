e61f62dca76e39ad921f937fcc06d1506bbe2dab
Nayem <ahnayem.sust@gmail.com>
2020-02-22 21:21:44 +0600

    random number

--- flutter_app/lib/app_screens/first_screen.dart
+++ flutter_app/lib/app_screens/first_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class FirstScreen extends StatelessWidget{
@@ -8,12 +10,20 @@ class FirstScreen extends StatelessWidget{
       color: Colors.lightBlueAccent,
       child: Center(
         child: Text(
-          "Hello Flutter",
+          generateLuckyNumber(),
           textDirection: TextDirection.ltr,
           style: TextStyle(color: Colors.white, fontSize: 40.0),
         ),
       ),
     );
+  }
+
+  String generateLuckyNumber() {
+
+    var random = Random();
+    int luckyNumber = random.nextInt(10);
+
+    return "Your lucky number is $luckyNumber";
   }
 
 }
