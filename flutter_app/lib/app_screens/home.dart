e6de5bddf901aa02076550773b522160aeeecff1
Nayem <ahnayem.sust@gmail.com>
2020-02-27 00:22:43 +0600

    padding

--- flutter_app/lib/app_screens/home.dart
+++ flutter_app/lib/app_screens/home.dart
@@ -7,29 +7,61 @@ class Home extends StatelessWidget {
       child: Container(
           alignment: Alignment.center,
           color: Colors.deepPurple,
-//        margin: EdgeInsets.only(left: 14.0),
-          child: Row(
+          padding: EdgeInsets.only(left: 10.0, top: 40.0),
+          child: Column(
             children: <Widget>[
-              Expanded(child: Text(
-                "Flight",
-                textDirection: TextDirection.ltr,
-                style: TextStyle(
-                    decoration: TextDecoration.none,
-                    fontSize: 35.0,
-                    fontFamily: 'Raleway',
-                    fontWeight: FontWeight.w700,
-                    color: Colors.white),
-              )),
-              Expanded(child: Text(
-                " I want to go by Flight",
-                textDirection: TextDirection.ltr,
-                style: TextStyle(
-                    decoration: TextDecoration.none,
-                    fontSize: 35.0,
-                    fontFamily: 'Raleway',
-                    fontWeight: FontWeight.w700,
-                    color: Colors.white),
-              )),
+              Row(
+                children: <Widget>[
+                  Expanded(
+                      child: Text(
+                    "Flight",
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                        decoration: TextDecoration.none,
+                        fontSize: 35.0,
+                        fontFamily: 'Raleway',
+                        fontWeight: FontWeight.w700,
+                        color: Colors.white),
+                  )),
+                  Expanded(
+                      child: Text(
+                    " I want to go by Flight",
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                        decoration: TextDecoration.none,
+                        fontSize: 20.0,
+                        fontFamily: 'Raleway',
+                        fontWeight: FontWeight.w700,
+                        color: Colors.white),
+                  )),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  Expanded(
+                      child: Text(
+                        "Bus",
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                            decoration: TextDecoration.none,
+                            fontSize: 35.0,
+                            fontFamily: 'Raleway',
+                            fontWeight: FontWeight.w700,
+                            color: Colors.white),
+                      )),
+                  Expanded(
+                      child: Text(
+                        " I want to go by Bus",
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                            decoration: TextDecoration.none,
+                            fontSize: 20.0,
+                            fontFamily: 'Raleway',
+                            fontWeight: FontWeight.w700,
+                            color: Colors.white),
+                      )),
+                ],
+              )
             ],
           )),
     );
