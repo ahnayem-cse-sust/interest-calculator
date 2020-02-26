8811448195307a7d9ee1f9aaae26ab0512b7963a
Nayem <ahnayem.sust@gmail.com>
2020-02-26 23:41:54 +0600

    expanded widget

--- flutter_app/lib/app_screens/home.dart
+++ flutter_app/lib/app_screens/home.dart
@@ -1,28 +1,37 @@
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-
     return Center(
       child: Container(
-        alignment: Alignment.center,
-        color: Colors.deepPurple,
+          alignment: Alignment.center,
+          color: Colors.deepPurple,
 //        margin: EdgeInsets.only(left: 14.0),
-        child: Text(
-            "Flight",
-            textDirection: TextDirection.ltr,
-            style: TextStyle(
-              decoration: TextDecoration.none,
-              fontSize: 75.0,
-              fontFamily: 'Raleway',
-              fontWeight: FontWeight.w700,
-              color: Colors.white
-            ),
-        ),
-      ),
+          child: Row(
+            children: <Widget>[
+              Expanded(child: Text(
+                "Flight",
+                textDirection: TextDirection.ltr,
+                style: TextStyle(
+                    decoration: TextDecoration.none,
+                    fontSize: 35.0,
+                    fontFamily: 'Raleway',
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white),
+              )),
+              Expanded(child: Text(
+                " I want to go by Flight",
+                textDirection: TextDirection.ltr,
+                style: TextStyle(
+                    decoration: TextDecoration.none,
+                    fontSize: 35.0,
+                    fontFamily: 'Raleway',
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white),
+              )),
+            ],
+          )),
     );
   }
-
 }
