ee9187720bcb2b976cd141037a706048b3f91339
Nayem <ahnayem.sust@gmail.com>
2020-02-28 19:20:19 +0600

    Long list

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,45 +1,34 @@
 import 'package:flutter/material.dart';
-
 import 'app_screens/home.dart';
 
 void main() {
   runApp(MaterialApp(
     title: "Exploring UI widgets",
     home: Scaffold(
-      appBar: AppBar(title: Text("Basic list view"),),
+      appBar: AppBar(
+        title: Text("Long list"),
+      ),
       body: getListView(),
     ),
   ));
 }
 
-Widget getListView() {
-  var listView = ListView(
-    children: <Widget>[
-      ListTile(
-        leading: Icon(Icons.landscape),
-        title: Text("Landscape"),
-        subtitle: Text("Beautiful View !"),
-        trailing: Icon(Icons.wb_sunny),
-        onTap: (){
-          debugPrint("Landscape tapped");
-        },
-      ),
-      ListTile(
-        leading: Icon(Icons.laptop_chromebook),
-        title: Text("Windows"),
-      ),
-      ListTile(
-        leading: Icon(Icons.phone),
-        title: Text("Phone"),
-      ),
-
-      Text("Yet another element in List"),
-
-      Container(color: Colors.red, height: 50.0,)
-    ],
-  );
-
-  return listView;
+List<String> getListElements() {
+  var items = List<String>.generate(1000, (counter) => "Item $counter");
+  return items;
 }
 
+Widget getListView() {
+  var listItems = getListElements();
 
+  var listView = ListView.builder(itemBuilder: (context, index) {
+    return ListTile(
+      leading: Icon(Icons.arrow_right),
+      title: Text(listItems[index]),
+      onTap: (){
+        debugPrint('${listItems[index]} was tapped');
+      },
+    );
+  });
+  return listView;
+}
