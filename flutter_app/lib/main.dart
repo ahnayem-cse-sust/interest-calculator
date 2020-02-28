10a155627ab97d4a07eefdfa82d0b7349d8a4b0d
Nayem <ahnayem.sust@gmail.com>
2020-02-28 13:50:52 +0600

    basic list view

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -2,10 +2,44 @@ import 'package:flutter/material.dart';
 
 import 'app_screens/home.dart';
 
-
-void main(){
+void main() {
   runApp(MaterialApp(
     title: "Exploring UI widgets",
-    home: Home(),
+    home: Scaffold(
+      appBar: AppBar(title: Text("Basic list view"),),
+      body: getListView(),
+    ),
   ));
 }
+
+Widget getListView() {
+  var listView = ListView(
+    children: <Widget>[
+      ListTile(
+        leading: Icon(Icons.landscape),
+        title: Text("Landscape"),
+        subtitle: Text("Beautiful View !"),
+        trailing: Icon(Icons.wb_sunny),
+        onTap: (){
+          debugPrint("Landscape tapped");
+        },
+      ),
+      ListTile(
+        leading: Icon(Icons.laptop_chromebook),
+        title: Text("Windows"),
+      ),
+      ListTile(
+        leading: Icon(Icons.phone),
+        title: Text("Phone"),
+      ),
+
+      Text("Yet another element in List"),
+
+      Container(color: Colors.red, height: 50.0,)
+    ],
+  );
+
+  return listView;
+}
+
+
