93aa88701cbeb9109e6e3337b1f55cc0ec7b325c
Nayem <ahnayem.sust@gmail.com>
2020-02-29 17:51:47 +0600

    statefull structure

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,55 +1,32 @@
 import 'package:flutter/material.dart';
-import 'app_screens/home.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: "Exploring UI widgets",
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text("Long list"),
-      ),
-      body: getListView(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          debugPrint("FAB Clicked");
-        },
-        child: Icon(Icons.add),
-        tooltip: 'Add One More Team',
-      ),
-    ),
-  ));
-}
 
-void showSnackBar(BuildContext context,String item) {
-  var snackBar = SnackBar(
-    content: Text("You just tapped $item"),
-    action: SnackBarAction(
-      label: "UNDO",
-      onPressed: (){
-        debugPrint('Performing dummy UNDO operation');
-      },
-    ),
+  runApp(
+    MaterialApp(
+      title: "Statefull App Example",
+      home: FavouriteCity(),
+    )
   );
-
-  Scaffold.of(context).showSnackBar(snackBar);
 }
 
-List<String> getListElements() {
-  var items = List<String>.generate(1000, (counter) => "Item $counter");
-  return items;
+class FavouriteCity extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _FavouriteCityState();
+  }
 }
+class _FavouriteCityState extends State<FavouriteCity> {
 
-Widget getListView() {
-  var listItems = getListElements();
+  String nameCity = "";
 
-  var listView = ListView.builder(itemBuilder: (context, index) {
-    return ListTile(
-      leading: Icon(Icons.arrow_right),
-      title: Text(listItems[index]),
-      onTap: (){
-        showSnackBar(context, listItems[index]);
-      },
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Stateful app example"),
+      ),
     );
-  });
-  return listView;
+  }
+
 }
