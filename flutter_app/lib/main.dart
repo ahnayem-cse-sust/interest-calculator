1c4a4f34c402fea3d0a68041ecf5b26e50c65ac5
Nayem <ahnayem.sust@gmail.com>
2020-02-29 18:48:02 +0600

    sateful widget

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
 
 void main() {
-
-  runApp(
-    MaterialApp(
-      title: "Statefull App Example",
-      home: FavouriteCity(),
-    )
-  );
+  runApp(MaterialApp(
+    title: "Statefull App Example",
+    home: FavouriteCity(),
+  ));
 }
 
 class FavouriteCity extends StatefulWidget {
@@ -16,17 +13,40 @@ class FavouriteCity extends StatefulWidget {
     return _FavouriteCityState();
   }
 }
-class _FavouriteCityState extends State<FavouriteCity> {
 
+class _FavouriteCityState extends State<FavouriteCity> {
   String nameCity = "";
 
   @override
   Widget build(BuildContext context) {
+
+    debugPrint("Favourite City Widget is created");
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Stateful app example"),
       ),
+      body: Container(
+        margin: EdgeInsets.all(20.0),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              onSubmitted: (String userInput) {
+                setState(() {
+                  debugPrint("set State is called, this tells framework to redraw the FavCity widget");
+                  nameCity = userInput;
+                });
+              },
+            ),
+            Padding(
+                padding: EdgeInsets.all(30.0),
+                child: Text(
+                  "Your bext city is $nameCity",
+                  style: TextStyle(fontSize: 20.0),
+                ))
+          ],
+        ),
+      ),
     );
   }
-
 }
