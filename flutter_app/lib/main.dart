49533105bc948eda8236c2d5a5650824369325a5
Nayem <ahnayem.sust@gmail.com>
2020-03-02 00:49:10 +0600

    Int

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,72 +1,28 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: "Statefull App Example",
-    home: FavouriteCity(),
-  ));
+
+  runApp(
+    MaterialApp(
+      title: 'Simple Interest Calculator App',
+      home: SIForm(),
+    )
+  );
 }
 
-class FavouriteCity extends StatefulWidget {
+class SIForm extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    return _FavouriteCityState();
+    return _SIFormState();
   }
 }
 
-class _FavouriteCityState extends State<FavouriteCity> {
-  String nameCity = "";
-  var _currencies = ['Taka', 'Rupees', 'Dollar', 'Pounds'];
-  var _currentItemSelected = 'Taka';
+class _SIFormState extends State<Form> {
+
+  var _currencies = ['Taka','Rupees','Dollars','Pounds'];
 
   @override
   Widget build(BuildContext context) {
-    debugPrint("Favourite City Widget is created");
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Stateful app example"),
-      ),
-      body: Container(
-        margin: EdgeInsets.all(20.0),
-        child: Column(
-          children: <Widget>[
-            TextField(
-              onSubmitted: (String userInput) {
-                setState(() {
-                  debugPrint(
-                      "set State is called, this tells framework to redraw the FavCity widget");
-                  nameCity = userInput;
-                });
-              },
-            ),
-            DropdownButton<String>(
-              items: _currencies.map((String dropDownStringItem) {
-                return DropdownMenuItem(
-                  value: dropDownStringItem,
-                  child: Text(dropDownStringItem),
-                );
-              }).toList(),
-              onChanged: (String newValueSelected) {
-                _onDropDownItemSelected(newValueSelected);
-              },
-              value: _currentItemSelected,
-            ),
-            Padding(
-                padding: EdgeInsets.all(30.0),
-                child: Text(
-                  "Your bext city is $nameCity",
-                  style: TextStyle(fontSize: 20.0),
-                ))
-          ],
-        ),
-      ),
-    );
-  }
-
-  void _onDropDownItemSelected(String newValueSelected) {
-    setState(() {
-      this._currentItemSelected = newValueSelected;
-    });
+    return null;
   }
 }
