f79ee502a895b50241c7f1c2c194493f4d9fa304
Nayem <ahnayem.sust@gmail.com>
2020-03-05 21:57:49 +0600

    test

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -17,12 +17,33 @@ class SIForm extends StatefulWidget {
   }
 }
 
-class _SIFormState extends State<Form> {
+class _SIFormState extends State<SIForm> {
 
   var _currencies = ['Taka','Rupees','Dollars','Pounds'];
+  final _minimumPadding = 5.0;
 
   @override
   Widget build(BuildContext context) {
-    return null;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Simple Interest Calculator'),
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+
+            getImageAsset(),
+
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget getImageAsset() {
+    AssetImage assetImage = AssetImage('images/us-dollar.png');
+    Image image = Image(image: assetImage, width: 125.0, height: 125.0);
+
+    return Container(child: image,margin: EdgeInsets.all(_minimumPadding * 10),);
   }
 }
