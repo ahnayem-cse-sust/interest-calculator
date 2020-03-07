698e4a02bc7dd0f7df0bfff60043d2d91fb6c0a4
Nayem <ahnayem.sust@gmail.com>
2020-03-07 19:53:31 +0600

    input field

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
 
 void main() {
-
-  runApp(
-    MaterialApp(
-      title: 'Simple Interest Calculator App',
-      home: SIForm(),
-    )
-  );
+  runApp(MaterialApp(
+    title: 'Simple Interest Calculator App',
+    home: SIForm(),
+  ));
 }
 
 class SIForm extends StatefulWidget {
@@ -18,8 +15,7 @@ class SIForm extends StatefulWidget {
 }
 
 class _SIFormState extends State<SIForm> {
-
-  var _currencies = ['Taka','Rupees','Dollars','Pounds'];
+  var _currencies = ['Taka', 'Rupees', 'Dollars', 'Pounds'];
   final _minimumPadding = 5.0;
 
   @override
@@ -31,9 +27,29 @@ class _SIFormState extends State<SIForm> {
       body: Container(
         child: Column(
           children: <Widget>[
-
             getImageAsset(),
-
+            Padding(
+              padding: EdgeInsets.only(top: _minimumPadding,bottom: _minimumPadding),
+              child: TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: 'Principal',
+                    hintText: 'Enter Principal e.g. 12000',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5.0))),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: _minimumPadding,bottom: _minimumPadding),
+              child: TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: 'Rate of Interest',
+                    hintText: 'In percent',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5.0))),
+              ),
+            ),
           ],
         ),
       ),
@@ -44,6 +60,10 @@ class _SIFormState extends State<SIForm> {
     AssetImage assetImage = AssetImage('images/us-dollar.png');
     Image image = Image(image: assetImage, width: 125.0, height: 125.0);
 
-    return Container(child: image,margin: EdgeInsets.all(_minimumPadding * 10),);
+    return Container(
+      child: image,
+      margin:
+          EdgeInsets.only(left: _minimumPadding * 3, top: _minimumPadding * 10),
+    );
   }
 }
