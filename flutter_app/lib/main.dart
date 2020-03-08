a6e9b434a213646a89c9794eef450511de90868a
Nayem <ahnayem.sust@gmail.com>
2020-03-08 23:51:16 +0600

    alpha version

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -6,10 +6,9 @@ void main() {
     title: 'Simple Interest Calculator App',
     home: SIForm(),
     theme: ThemeData(
-      brightness: Brightness.dark,
-      primaryColor: Colors.indigo,
-      accentColor: Colors.indigoAccent
-    ),
+        brightness: Brightness.dark,
+        primaryColor: Colors.indigo,
+        accentColor: Colors.indigoAccent),
   ));
 }
 
@@ -30,9 +29,10 @@ class _SIFormState extends State<SIForm> {
   TextEditingController roiController = TextEditingController();
   TextEditingController termController = TextEditingController();
 
+  var displayResult = '';
+
   @override
   Widget build(BuildContext context) {
-
     TextStyle textStyle = Theme.of(context).textTheme.title;
 
     return Scaffold(
@@ -112,23 +112,38 @@ class _SIFormState extends State<SIForm> {
               ),
             ),
             Padding(
-              padding: EdgeInsets.only(top: _minimumPadding,bottom: _minimumPadding),
+              padding: EdgeInsets.only(
+                  top: _minimumPadding, bottom: _minimumPadding),
               child: Row(
                 children: <Widget>[
                   Expanded(
                     child: RaisedButton(
                       color: Theme.of(context).accentColor,
                       textColor: Theme.of(context).primaryColorDark,
-                      child: Text('Calculate', textScaleFactor: 1.5,),
-                      onPressed: () {},
+                      child: Text(
+                        'Calculate',
+                        textScaleFactor: 1.5,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          this.displayResult = _calculateTotalReturns();
+                        });
+                      },
                     ),
                   ),
                   Expanded(
                     child: RaisedButton(
                       color: Theme.of(context).primaryColorDark,
                       textColor: Theme.of(context).primaryColorLight,
-                      child: Text('Reset', textScaleFactor: 1.5,),
-                      onPressed: () {},
+                      child: Text(
+                        'Reset',
+                        textScaleFactor: 1.5,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _reset();
+                        });
+                      },
                     ),
                   )
                 ],
@@ -136,7 +151,10 @@ class _SIFormState extends State<SIForm> {
             ),
             Padding(
               padding: EdgeInsets.all(_minimumPadding * 2),
-              child: Text('Todo Text'),
+              child: Text(
+                this.displayResult,
+                style: textStyle,
+              ),
             ),
           ],
         ),
@@ -155,9 +173,30 @@ class _SIFormState extends State<SIForm> {
     );
   }
 
-  void _onDropDownItemSelected(String newValueSelected){
+  void _onDropDownItemSelected(String newValueSelected) {
     setState(() {
       this._currentItemSelected = newValueSelected;
     });
+  }
+
+  String _calculateTotalReturns() {
+    double principal = double.parse(principalController.text);
+    double roi = double.parse(roiController.text);
+    double term = double.parse(termController.text);
+
+    double totalAmountPayable = principal + (principal * roi * term) / 100;
+
+    String result =
+        'After $term years, your investment will be worth $totalAmountPayable $_currentItemSelected';
+
+    return result;
+  }
+
+  void _reset() {
+    principalController.text = '';
+    roiController.text = '';
+    termController.text = '';
+    displayResult = '';
+    _currentItemSelected = _currencies[0];
   }
 }
